95fbf47ba411463c1db401dc02a5b0f99fdaf041
Anil Kumawat <anilkumawat7896@gmail.com>
2024-11-24 14:55:14 +0530

    customizing component done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,11 @@
 import type { Config } from "tailwindcss";
-
 const config: Config = {
     darkMode: ["class"],
     content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
   	extend: {
@@ -58,6 +58,7 @@ const config: Config = {
   		}
   	}
   },
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
   plugins: [require("tailwindcss-animate")],
 };
 export default config;
